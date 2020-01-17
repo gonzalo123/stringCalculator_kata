31a42653ed07ebc99302ae0e0d9fa0b35783c5fe
gonzalo123 <gonzalo123@gmail.com>
2020-01-17 16:19:25 +0100

    wip

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -6,7 +6,7 @@ class StringCalculatorTest extends TestCase
 {
     function testBar()
     {
-        $foo = new StringCalculator();
-        $this->assertEquals(0, $foo->bar(), "first test must be red");
+        $stringCalculator = new StringCalculator();
+        $this->assertEquals(0, $stringCalculator->bar(), "first test must be red");
     }
 }
